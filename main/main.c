065be8bcaf76d871fc9957db1dff48a6069e8208
Shankari02 <shankari.ak0208@gmail.com>
2024-04-06 17:17:27 +0530

    Added files

--- main/main.c
+++ main/main.c
@@ -8,10 +8,11 @@
 #include <nvs_flash.h>
 #include <sys/param.h>
 #include <esp_wifi.h>
+//#include <esp_log.h>
 
 #define WIFI_SSID "ESP32 OTA Update"
-#define WIFI_PASSWORD "linux"
-
+#define WIFI_PASSWORD "lovelinux"
+//static const char *TAG = "JMD";
 /*
  * Serve OTA update portal (index.html)
  */
@@ -30,11 +31,10 @@ esp_err_t index_get_handler(httpd_req_t *req)
 esp_err_t update_post_handler(httpd_req_t *req)
 {
 	
-
 	char buf[1000];
 	esp_ota_handle_t ota_handle;
 	int remaining = req->content_len;
-
+	
 	const esp_partition_t *ota_partition = esp_ota_get_next_update_partition(NULL);
 	ESP_ERROR_CHECK(esp_ota_begin(ota_partition, OTA_SIZE_UNKNOWN, &ota_handle));
 
